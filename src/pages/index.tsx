b61741f13455dcff9ee1497e83cdff0599144178
Gavin Pai <gavinpai@hotmail.com>
2025-04-22 12:09:02 -0700

    Adding first coordinate from events to map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { NextPage } from "next";
+import type { User } from '@supabase/supabase-js'
+import type { GetServerSidePropsContext } from 'next'
+import { createClient } from '@/supabase/server-props'
 import MapView from "@/views";
 
 const Homepage: NextPage = (props) => {
@@ -9,9 +12,20 @@ const Homepage: NextPage = (props) => {
         <title>⌘</title>
       </Head>
       { /* Main view will go here */}
-      <MapView />
+      <MapView data={props["events_v0"]} />
     </div>
   )
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const supabase = createClient(context)
+  const { data: events_v0 } = await supabase.from("events_v0").select();
+  console.log(events_v0);
+  return {
+    props: {
+      events_v0: events_v0,
+    },
+  }
+}
 
 export default Homepage;
