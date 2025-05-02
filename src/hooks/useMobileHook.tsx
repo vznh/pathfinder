d3ea3a1706c08aadc2ac1b38c0d9a83f7101df67
Jason Son <jasonson2004@gmail.com>
2025-05-02 12:52:06 -0700

    add: mobile detection hook for abstracted use

---
+++ src/hooks/useMobileHook.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Check on initial load
+    checkIfMobile();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", checkIfMobile);
+
+    // Clean up
+    return () => window.removeEventListener("resize", checkIfMobile);
+  }, []);
+
+  return isMobile;
+}
