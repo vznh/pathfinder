2577dbfacf3205b243dceb129b198d4e05c7bc58
Aneesh Komaragiri <aneesh.komaragiri2004@gmail.com>
2025-05-15 21:39:55 -0700

    search bar nav implemented

--- src/views/type/desktop.tsx
+++ src/views/type/desktop.tsx
@@ -115,22 +115,6 @@ const DesktopView: React.FC = () => {
     mapboxClient.camera.zoomTo(s.coordinates, 18, true);
   };
 
-  const events = useEventsStore(state => state.events);
-
-  useEffect(() => {
-    if (!events || events.length === 0) return;
-
-    for (const row of events) {
-      if (
-        row &&
-        typeof row.longitude === "number" &&
-        typeof row.latitude === "number"
-      ) {
-        mapboxClient.events.addEventMarker([row.longitude, row.latitude]);
-      }
-    }
-  }, [events]);
-
   useEffect(() => {
     if (waypointMode) {
       document.body.style.cursor = "crosshair";
@@ -248,8 +232,6 @@ const DesktopView: React.FC = () => {
         />
       )}
 
-      <DashboardLayout
-        development={false}
       <DashboardLayout
         development={false}
         onWaypointModeToggle={handleWaypointModeToggle}
