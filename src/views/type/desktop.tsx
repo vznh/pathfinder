3f2ea8f1afc0e85dc0b7cd29323311786f7adcbc
Jason Son <jasonson2004@gmail.com>
2025-06-03 22:08:09 -0700

    test: check without subscription to auth

--- src/views/type/desktop.tsx
+++ src/views/type/desktop.tsx
@@ -127,13 +127,6 @@ const DesktopView: React.FC = () => {
 
     fetchUser();
 
-    // Subscribe to auth state changes
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
-    });
-    return () => {
-      listener?.subscription?.unsubscribe();
-    };
   }, [supabase]);
 
 
